250e8dde05e05170f3ff8e4e6015ac43cf7ea86e
Edder Nain Chalen Silva <echalen2@unemi.edu.ec>
2025-10-09 01:16:56 -0500

    agrego el programa que suma

---
+++ PROGRAMACIÒN/Programa que suma.py
@@ -0,0 +1,11 @@
+# Programa que suma dos números ingresados por el usuario
+
+# Pedir los números al usuario
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+# Realizar la suma
+suma = numero1 + numero2
+
+# Mostrar el resultado
+print("La suma de", numero1, "y", numero2, "es:", suma)
